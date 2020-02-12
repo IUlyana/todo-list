975f9b5f9b6c8bc40792e91e79b7201b598a7695
kayday <kadaniilo@gmail.com>
2020-02-12 12:29:02 +0300

    Fixes semantics, typos, logic errors

--- todo-list-runner.js
+++ todo-list-runner.js
@@ -1,28 +1,14 @@
+// Немного кода за тебя уже написано.
 const fs = require('fs');
-const parse = require('csv-parse');
 
-
+// Аргументы командной строки.
 const args = process.argv.slice(2);
 console.log(`You've passed arguments: [${args.length ? args : 'no args :('}]`);
 console.log('---------------------------');
 
-
+// Достаём todos из файла.
 const filePath = 'todo-list-data.csv';
-const csvData = [];
-
-console.log('Here is your todos:');
-fs.createReadStream(filePath)
-    .pipe(parse({ delimiter: ':' }))
-    .on('data', function (csvrow) {
-        //do something with csvrow
-        csvData.push(csvrow[0]);
-    })
-    .on('end', function () {
-        //do something wiht csvData
-        console.log(
-            csvData
-                .slice(1)
-                .map(item => (`- ${item}`))
-                .join('\n')
-        );
-    });
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    const todos = data.split('\n').slice(1);
+    console.log('Here is your todos:', todos);
+});
